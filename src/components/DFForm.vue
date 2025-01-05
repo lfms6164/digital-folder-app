3ee4d6c861cf072ab389b78c5f6959890c1caee0
lfms6164 <morim.leonardo2@gmail.com>
2025-01-05 13:47:49 +0000

    Improve Form component, improve how form handles autocomplete component

--- src/components/DFForm.vue
+++ src/components/DFForm.vue
@@ -6,35 +6,52 @@
           :is="formComponentsMap[field.formRepresentation.component]"
           v-model="localModelValue[key]"
           :label="field.label"
-          :type="field.formRepresentation.type"
+          :type="(field.formRepresentation as InputValueProperties).type"
           :rules="field.formRepresentation.rule"
+          :items="autocompleteItems"
+          :item-title="'name'"
+          :item-value="(item: any) => item"
+          :multiple="true"
+          :chips="true"
+          :closable-chips="true"
           outlined
           clearable
+          hide-canvas
+          width="400"
+          hide-inputs
+          :modes="['hex']"
           density="compact"
           variant="outlined"
+          :disabled="loading"
         />
       </v-row>
     </div>
 
     <v-row justify="end" class="form-actions">
-      <v-btn text="Cancel" @click="emit('cancel')" />
-      <v-btn text="Submit" type="submit" />
+      <v-btn text="Cancel" variant="plain" :disabled="loading" @click="emit('cancel')" />
+      <v-btn text="Submit" variant="tonal" :disabled="loading" :loading="loading" type="submit" />
       <!-- Removed @click="submit" cos apparently not needed -->
     </v-row>
   </v-form>
 </template>
 
 <script setup lang="ts">
-import { ref, watch } from 'vue'
-import { GenericRepresentation } from '../models/Generic'
+import { onMounted, ref, watch } from 'vue'
+import {
+  GenericRepresentation,
+  AutocompleteProperties,
+  InputValueProperties
+} from '../models/Generic'
 import { cloneDeep } from 'lodash'
 
 const props = defineProps<{
   representation: GenericRepresentation<any>
   modelValue: any
+  loading: boolean
 }>()
 
 const localModelValue = ref(cloneDeep(props.modelValue))
+const autocompleteItems = ref<any[]>([])
 
 const emit = defineEmits(['update:modelValue', 'cancel', 'submit'])
 
@@ -57,4 +74,30 @@ const submit = async (event: any) => {
   const form = await event
   if (form.valid) emit('submit', localModelValue.value)
 }
+
+const findAutocompleteInForm = () => {
+  for (const key in props.representation) {
+    const formRepresentation = props.representation[key].formRepresentation
+    if (formRepresentation.component === 'Autocomplete') {
+      return key
+    }
+  }
+  return null
+}
+
+onMounted(async () => {
+  const field = findAutocompleteInForm()
+
+  if (field) {
+    const formRepresentation = props.representation[`${field}`]
+      .formRepresentation as AutocompleteProperties
+
+    try {
+      const response = await formRepresentation.listItems()
+      autocompleteItems.value = response
+    } catch (error) {
+      console.error('Error fetching items:', error)
+    }
+  }
+})
 </script>
