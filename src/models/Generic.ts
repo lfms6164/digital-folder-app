1fbcf8ee1ef199f043c2ae92234e7bdbd5695612
lfms6164 <morim.leonardo2@gmail.com>
2025-01-05 13:49:44 +0000

    Improve Generic Representation, component specific properties

--- src/models/Generic.ts
+++ src/models/Generic.ts
@@ -4,14 +4,43 @@ export type GenericRepresentation<Type> = {
 
 export type GenericProperties = {
   label: string
-  dataDialog: unknown
-  tableColumn: unknown
-  formRepresentation: formRepresentation
+  dataDialog: any
+  tableColumn: any
+  formRepresentation: FormRepresentation
 }
 
-interface formRepresentation {
+interface BaseFormRepresentation {
   component: 'Autocomplete' | 'CheckBox' | 'ColorPicker' | 'InputValue' | 'Hidden'
   row: number
   rule: any
+}
+
+type FormRepresentation =
+  | AutocompleteProperties
+  | CheckBoxProperties
+  | ColorPickerProperties
+  | HiddenProperties
+  | InputValueProperties
+
+export interface AutocompleteProperties extends BaseFormRepresentation {
+  component: 'Autocomplete'
+  listItems: any
+  multiple?: boolean
+}
+
+export interface CheckBoxProperties extends BaseFormRepresentation {
+  component: 'CheckBox'
+}
+
+export interface ColorPickerProperties extends BaseFormRepresentation {
+  component: 'ColorPicker'
+}
+
+export interface HiddenProperties extends BaseFormRepresentation {
+  component: 'Hidden'
+}
+
+export interface InputValueProperties extends BaseFormRepresentation {
+  component: 'InputValue'
   type?: 'password'
 }
