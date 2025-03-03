eaf4dfcbac5ac244bb2595f1ac9b5eaae5325552
lfms6164 <morim.leonardo2@gmail.com>
2025-03-03 12:06:20 +0000

    Create DFButton component

---
+++ src/components/DFBtn.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-tooltip :text="tooltip" location="top">
+    <template v-slot:activator="{ props }">
+      <v-btn
+        v-bind="tooltip ? props : null"
+        v-ripple="false"
+        :color="color"
+        :icon="icon"
+        :disabled="disabled"
+        :readonly="readonly"
+        variant="text"
+        @click="emit('onClick')"
+      />
+    </template>
+  </v-tooltip>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  tooltip?: string
+  color?: string
+  icon?: string
+  disabled?: boolean
+  readonly?: boolean
+}>()
+
+const emit = defineEmits(['onClick'])
+</script>
