a7950f7169bb256aa918e5f1bb19ed79453d7af6
lfms6164 <morim.leonardo2@gmail.com>
2025-03-03 18:14:41 +0000

    Improve DFCard, dynamic behavior according to window size

--- src/components/DFCard.vue
+++ src/components/DFCard.vue
@@ -25,22 +25,24 @@
     </v-card-title>
 
     <v-card-actions>
-      <v-chip-group v-for="chip in item.tags" :key="chip.id">
+      <v-chip-group>
         <v-chip
+          v-for="chip in item.tags"
+          :key="chip.id"
           :style="{ backgroundColor: chip.color }"
           :prepend-icon="`mdi-${chip.icon}`"
           :ripple="false"
           rounded="sm"
           variant="text"
         >
-          {{ item.tags.length > 5 ? '' : chip.name }}
+          {{ windowWidth < chipCompactState(item.tags!.length).value ? '' : chip.name }}
         </v-chip>
       </v-chip-group>
 
       <v-spacer />
       <DFBtn
-        v-if="item.description"
-        :tooltip="`${expand ? $t('action.close') : $t('action.open')} ${$t('entity.project.fields.description')}`"
+        v-if="item.introduction"
+        :tooltip="`${expand ? $t('action.close') : $t('action.open')} ${$t('entity.project.fields.introduction')}`"
         :icon="expand ? 'mdi-chevron-up' : 'mdi-chevron-down'"
         @on-click="expand = !expand"
       />
@@ -55,14 +57,15 @@
       <div v-show="expand">
         <v-divider />
 
-        <v-card-text> {{ item.description }} </v-card-text>
+        <v-card-text> {{ item.introduction }} </v-card-text>
       </div>
     </v-expand-transition>
   </v-card>
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from 'vue'
+import { computed, onMounted, onUnmounted, ref } from 'vue'
+import { ProjectOut } from '../api'
 import { useLoginStore } from '../stores/loginStore'
 import router from '../router'
 import DFBtn from '../components/DFBtn.vue'
@@ -72,11 +75,26 @@ const isAdmin = computed((): boolean => loginStore.isAdmin)
 
 const expand = ref<boolean>(false)
 const selected = ref<boolean>(false)
+const windowWidth = ref(window.innerWidth)
+
+const chipCompactState = (n_tags: number) => computed(() => 1000 + 200 * n_tags)
 
 const props = defineProps<{
-  item: any
+  item: ProjectOut
   selectedId?: string
 }>()
 
 const emit = defineEmits(['selected'])
+
+const updateWindowSize = () => {
+  windowWidth.value = window.innerWidth
+}
+
+onMounted(() => {
+  window.addEventListener('resize', updateWindowSize)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('resize', updateWindowSize)
+})
 </script>
