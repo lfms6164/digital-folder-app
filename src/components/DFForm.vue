9875b3f02419d214bb64a14b26bd1eec39138f21
lfms6164 <morim.leonardo2@gmail.com>
2025-03-03 12:10:28 +0000

    Improve DFForm to handle v-file-input component

--- src/components/DFForm.vue
+++ src/components/DFForm.vue
@@ -2,44 +2,71 @@
   <v-form ref="Form" @submit.prevent="submit">
     <div v-for="[key, field] in Object.entries(representation)" :key="key">
       <v-row class="mb-4" v-if="field.formRepresentation.component !== 'Hidden'">
-        <component
-          :is="formComponentsMap[field.formRepresentation.component]"
-          v-model="localModelValue[key]"
-          :label="field.label"
-          :type="(field.formRepresentation as InputValueProperties).type"
-          :rules="field.formRepresentation.rule"
-          :items="autocompleteItems"
-          :item-title="'name'"
-          :item-value="(item: any) => item"
-          :multiple="true"
-          :chips="true"
-          :closable-chips="true"
-          outlined
-          clearable
-          hide-canvas
-          width="400"
-          hide-inputs
-          :modes="['hex']"
-          density="compact"
-          variant="outlined"
-          :disabled="loading"
-        />
+        <template v-if="field.formRepresentation.component === 'FileInput'">
+          <v-file-input
+            v-model="fileInputValue[key]"
+            accept="image/*"
+            clearable
+            density="compact"
+            variant="solo-filled"
+            :label="$t(field.label)"
+            :prepend-icon="(field.formRepresentation as FileInputProperties).icon"
+            @update:modelValue="handleFileUpdate($event, key)"
+          />
+        </template>
+
+        <template v-else>
+          <component
+            :is="formComponentsMap[field.formRepresentation.component]"
+            v-model="localModelValue[key]"
+            :label="$t(field.label)"
+            :type="(field.formRepresentation as InputValueProperties).type"
+            :rules="field.formRepresentation.rule"
+            :items="autocompleteItems"
+            :item-title="'name'"
+            :item-value="(item: any) => item"
+            :multiple="true"
+            :chips="true"
+            :closable-chips="true"
+            outlined
+            clearable
+            hide-canvas
+            width="400"
+            hide-inputs
+            :modes="['hex']"
+            density="compact"
+            variant="outlined"
+            :disabled="loading"
+          />
+        </template>
       </v-row>
     </div>
 
     <v-row justify="end" class="form-actions">
-      <v-btn text="Cancel" variant="plain" :disabled="loading" @click="emit('cancel')" />
-      <v-btn text="Submit" variant="tonal" :disabled="loading" :loading="loading" type="submit" />
+      <v-btn
+        :text="$t('dialog.action.cancel')"
+        variant="plain"
+        :disabled="loading"
+        @click="emit('cancel')"
+      />
+      <v-btn
+        :text="$t('dialog.action.submit')"
+        variant="tonal"
+        :disabled="loading"
+        :loading="loading"
+        type="submit"
+      />
       <!-- Removed @click="submit" cos apparently not needed -->
     </v-row>
   </v-form>
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref, watch } from 'vue'
+import { computed, onMounted, ref, watch } from 'vue'
 import {
   GenericRepresentation,
   AutocompleteProperties,
+  FileInputProperties,
   InputValueProperties
 } from '../models/Generic'
 import { cloneDeep } from 'lodash'
@@ -67,6 +94,7 @@ const formComponentsMap = {
   Autocomplete: 'v-autocomplete',
   CheckBox: 'v-checkbox',
   ColorPicker: 'v-color-picker',
+  FileInput: 'v-file-input',
   InputValue: 'v-text-field'
 }
 
@@ -100,4 +128,20 @@ onMounted(async () => {
     }
   }
 })
+
+const fileInputValue = computed(() => {
+  return Object.keys(localModelValue.value).reduce((result: any, key) => {
+    result[key] =
+      typeof localModelValue.value[key] === 'string' &&
+      props.representation[key].formRepresentation.component === 'FileInput'
+        ? { name: localModelValue.value[key] }
+        : localModelValue.value[key]
+    return result
+  }, {})
+})
+
+const handleFileUpdate = (event: any, key: any) => {
+  fileInputValue.value[key] = event ?? null // Update file object
+  localModelValue.value[key] = event ?? null // Sync with modelValue
+}
 </script>
