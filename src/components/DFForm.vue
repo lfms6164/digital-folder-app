84c6e91a4e8ed105d2823d39de77eef4463622d2
lfms6164 <morim.leonardo2@gmail.com>
2025-03-06 18:37:07 +0000

    Improve DFForm to handle multiple fields per row

--- src/components/DFForm.vue
+++ src/components/DFForm.vue
@@ -1,44 +1,47 @@
 <template>
   <v-form ref="Form" @submit.prevent="submit">
-    <div v-for="[key, field] in Object.entries(representation)" :key="key">
-      <v-row class="mb-4" v-if="field.formRepresentation.component !== 'Hidden'">
-        <template v-if="field.formRepresentation.component === 'FileInput'">
-          <v-file-input
-            v-model="fileInputValue[key]"
-            accept="image/*"
-            clearable
-            density="compact"
-            variant="solo-filled"
-            :label="$t(field.label)"
-            :prepend-icon="(field.formRepresentation as FileInputProperties).icon"
-            @update:modelValue="handleFileUpdate($event, key)"
-          />
-        </template>
+    <div v-for="[row, fields] in groupedFormFieldsByRow" :key="row">
+      <v-row>
+        <v-col v-for="{ key, field } in fields" :key="key" :cols="12 / fields.length">
+          <template v-if="field.formRepresentation.component !== 'Hidden'">
+            <template v-if="field.formRepresentation.component === 'FileInput'">
+              <v-file-input
+                v-model="fileInputValue[key]"
+                accept="image/*"
+                clearable
+                density="compact"
+                variant="solo-filled"
+                :label="$t(field.label)"
+                :prepend-icon="(field.formRepresentation as FileInputProperties).icon"
+                @update:modelValue="handleFileUpdate($event, key)"
+              />
+            </template>
 
-        <template v-else>
-          <component
-            :is="formComponentsMap[field.formRepresentation.component]"
-            v-model="localModelValue[key]"
-            :label="$t(field.label)"
-            :type="(field.formRepresentation as InputValueProperties).type"
-            :rules="field.formRepresentation.rule"
-            :items="autocompleteItems"
-            :item-title="'name'"
-            :item-value="(item: any) => item"
-            :multiple="true"
-            :chips="true"
-            :closable-chips="true"
-            outlined
-            clearable
-            hide-canvas
-            width="400"
-            hide-inputs
-            :modes="['hex']"
-            density="compact"
-            variant="outlined"
-            :disabled="loading"
-          />
-        </template>
+            <template v-else>
+              <component
+                :is="formComponentsMap[field.formRepresentation.component]"
+                v-model="localModelValue[key]"
+                :label="$t(field.label)"
+                :type="(field.formRepresentation as InputValueProperties).type"
+                :rules="field.formRepresentation.rule"
+                :items="autocompleteItems"
+                :item-title="'name'"
+                :item-value="(item: any) => item"
+                :multiple="true"
+                :chips="true"
+                :closable-chips="true"
+                outlined
+                clearable
+                hide-canvas
+                hide-inputs
+                :modes="['hex']"
+                density="compact"
+                variant="outlined"
+                :disabled="loading"
+              />
+            </template>
+          </template>
+        </v-col>
       </v-row>
     </div>
 
@@ -56,7 +59,6 @@
         :loading="loading"
         type="submit"
       />
-      <!-- Removed @click="submit" cos apparently not needed -->
     </v-row>
   </v-form>
 </template>
@@ -64,9 +66,10 @@
 <script setup lang="ts">
 import { computed, onMounted, ref, watch } from 'vue'
 import {
-  GenericRepresentation,
   AutocompleteProperties,
   FileInputProperties,
+  GenericProperties,
+  GenericRepresentation,
   InputValueProperties
 } from '../models/Generic'
 import { cloneDeep } from 'lodash'
@@ -77,18 +80,10 @@ const props = defineProps<{
   loading: boolean
 }>()
 
-const localModelValue = ref(cloneDeep(props.modelValue))
-const autocompleteItems = ref<any[]>([])
-
 const emit = defineEmits(['update:modelValue', 'cancel', 'submit'])
 
-watch(
-  () => localModelValue.value,
-  () => {
-    emit('update:modelValue', localModelValue.value)
-  },
-  { deep: true }
-)
+const localModelValue = ref(cloneDeep(props.modelValue))
+const autocompleteItems = ref<any[]>([])
 
 const formComponentsMap = {
   Autocomplete: 'v-autocomplete',
@@ -99,10 +94,43 @@ const formComponentsMap = {
   TextArea: 'v-textarea'
 }
 
-const submit = async (event: any) => {
-  const form = await event
-  if (form.valid) emit('submit', localModelValue.value)
-}
+const groupedFormFieldsByRow = computed(() => {
+  /*
+Output:
+[
+  [
+    "0",
+    [
+      { key: "name", field: { formRepresentation: { row: 1, component: "InputValue" } } },
+      { key: "icon", field: { ... } }
+    ]
+  ],
+  [
+    "1",
+    [
+      { key: "color", field: { ... } }
+    ]
+  ]
+]
+*/
+  const rows: Record<number, { key: string; field: GenericProperties }[]> = {}
+
+  Object.entries(props.representation).forEach(([key, field]) => {
+    const row = field.formRepresentation.row
+    rows[row] = rows[row] ?? []
+    rows[row].push({ key, field })
+  })
+
+  return Object.entries(rows).sort(([x], [y]) => Number(x) - Number(y))
+})
+
+watch(
+  () => localModelValue.value,
+  () => {
+    emit('update:modelValue', localModelValue.value)
+  },
+  { deep: true }
+)
 
 const findAutocompleteInForm = () => {
   for (const key in props.representation) {
@@ -144,5 +172,10 @@ const fileInputValue = computed(() => {
 const handleFileUpdate = (event: any, key: any) => {
   fileInputValue.value[key] = event ?? null // Update file object
   localModelValue.value[key] = event ?? null // Sync with modelValue
+}
+
+const submit = async (event: any) => {
+  const form = await event
+  if (form.valid) emit('submit', localModelValue.value)
 }
 </script>
